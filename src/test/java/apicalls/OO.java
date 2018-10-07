dbc8ee828f849c10b30d0ad126442ca61fd26c2e
S00344 <S00344@MC01LAP0411.Mercator.com>
2018-10-07 17:34:30 +0400

    new changes

--- src/test/java/apicalls/OO.java
+++ src/test/java/apicalls/OO.java
@@ -1,6 +1,14 @@
 package apicalls;
 
 import org.testng.annotations.*;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
 import io.restassured.response.*;
 import static io.restassured.RestAssured.*;
 import java.io.IOException;
@@ -8,42 +16,99 @@ import java.io.IOException;
 public class OO extends genericFunctions{
 
 	String token;
+	ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("extent.html");
+	ExtentReports extent = new ExtentReports();
+
 
 	@BeforeSuite
 	public void getToken() throws IOException {
 		token=GetBearerID();
+		extent.attachReporter(htmlReporter);
 	}
 
-	@Test(priority=1)
+	@Test(priority=1,enabled=false)
 	public void TransportLeg() throws IOException{
 		baseURI=getProperty("BaseURL_service");
-		Response res= given().log().uri().
-				header("x-tenant","XX").
-				header("Authorization","Bearer "+token).
-				param("boardPoint", "DXB").
-				param("offPoint", "BOM").
-				param("date", "1534310936000").
-				when().get("services/transport/details").then().assertThat().statusCode(200).extract().response();
-		String r=res.asString();
-		System.out.println(r);
-		/*JsonPath js= new JsonPath(r);
-		refresh_token=js.get("refresh_token");
-		System.out.println(refresh_token);*/
+		ExtentTest test = extent.createTest("TransportLeg", "To List all the Flights Between DXB-BOM");
+		test.log(Status.INFO,"Bearer Token Passed:"+token);
+		test.log(Status.INFO,"Base URL:"+baseURI+"/services/transport/details");
+
+		Response res;
+		try {
+			res= given().log().ifValidationFails().
+					header("x-tenant","XX").
+					header("Authorization","Bearer "+token).
+					param("boardPoint", "DXB").
+					param("offPoint", "BOM").
+					param("date", "1534310936000").
+					when().get("services/transport/details").then().assertThat().statusCode(200).extract().response();
+			test.info("Response from the Server : "+res.headers());
+			test.info("Response from the Server : "+res.asString());
+			test.log(Status.PASS, "Asserion passed");
+
+		}
+		catch(java.lang.AssertionError e) {
+			test.log(Status.FAIL, "Assertion failed (Response,"+e+")");
+		}
+		extent.flush();
 	}
-	
-	@Test(priority=2)
+
+	@Test(priority=2,enabled=false)
 	public void ListCitiesinCountry() throws IOException{
 		baseURI=getProperty("MDM_URL");
-		Response res= given().log().uri().
-				header("x-tenant","XX").
-				header("Authorization","Bearer "+token).
-				when().get(getProperty("ListCitiesinCountry_service")+"/AE").then().assertThat().statusCode(200).extract().response();
-		String r=res.asString();
-		System.out.println(r);
-		/*JsonPath js= new JsonPath(r);
-	refresh_token=js.get("refresh_token");
-	System.out.println(refresh_token);*/
+		ExtentTest test = extent.createTest("ListCitiesinCountry", "Sample description");
+		test.info("Request URL: "+baseURI+getProperty("ListCitiesinCountry_service")+"/AE");
+
+		try {
+			Response res= given().log().uri().
+					header("x-tenant","XX").
+					header("Authorization","Bearer "+token).
+					when().get(getProperty("ListCitiesinCountry_service")+"/AE").then().assertThat().statusCode(200).extract().response();
+			test.info("Response from the Server : "+res.asString());
+			test.log(Status.PASS, "Asserion passed");
+			System.out.println(res.asString());
+		}
+		catch(java.lang.AssertionError e) {
+			test.log(Status.FAIL, "Assertion failed (Response,"+e+")");
+		}
+		extent.flush();
 	}
+
+	@Test(priority=3,enabled=false)
+	public void Carrier() throws IOException{
+		baseURI=getProperty("MDM_URL");
+		ExtentTest test = extent.createTest("Carrier", "Details of Carrier");
+		test.info("Request URL: "+baseURI+getProperty("Carrier_service"));
+		try {
+			Response res= given().log().ifValidationFails().
+					header("Authorization","Bearer "+token).
+					when().get(getProperty("Carrier_service")).then().assertThat().statusCode(200).extract().response();
+			test.info("Response from the Server : "+res.asString());
+			test.log(Status.PASS, "Asserion passed");
+			System.out.println(res.asString());
+		}
+		catch(java.lang.AssertionError e) {
+			test.log(Status.FAIL, "Assertion failed (Response,"+e+")");
+			System.out.println(e);
+		}
+		extent.flush();
+	}
+
+
+	@Test(priority=4)
+	public void offerSearch() throws IOException{
+		baseURI="http://10.4.11.108:8093";
+			Response res= given().log().all().
+					header("Authorization","Bearer "+token).
+					header("x-tenant","XX").
+					header("Content-Type","application/json").
+					body("{ \"useAllotment\": false, \"commodity\": { \"commodityCode\": \"9203\" }, \"shcs\": [], \"cargoType\": \"F\", \"sortType\": 1, \"scheduleParams\": { \"flights\": [], \"bodyTypes\": [], \"departureDate\": 1530043200000, \"origin\": { \"code\": \"DXB\" }, \"destination\": { \"code\": \"BOM\" }, \"flightExactMatch\": false }, \"spaceParams\": { \"weight\": { \"unit\": { \"code\": \"K\" }, \"value\": 5 }, \"volume\": { \"unit\": { \"code\": \"CM\" }, \"value\": 0.029999 }, \"pieces\": \"5\" }, \"priceParams\": { \"productCode\": \"GCR\", \"mop\": \"PP\", \"declaredValue\": { \"amount\": 0, \"currency\": \"AED\" } }, \"document\": { \"documentType\": \"AWB\", \"documentPrefix\": \"888\", \"documentNumber\": \"UNK\" }, \"goodsDescription\": \"TOYS. GAMES\", \"pageInfo\": { \"pageNumber\": 1, \"pageSize\": \"15\" } }").
+					when().post("/services/cargo/offers").then().assertThat().statusCode(200).extract().response();
+			
+			System.out.println(res.asString());
+		
+	}
+
 
 
 }
--- src/test/java/apicalls/genericFunctions.java
+++ src/test/java/apicalls/genericFunctions.java
@@ -3,6 +3,7 @@ package apicalls;
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -19,7 +20,7 @@ public class genericFunctions {
 	
 	public static String GetBearerID() throws IOException {
 		baseURI=getProperty("BaseURL_outh");
-		Response res= given().log().uri().
+		Response res= given().log().ifValidationFails().
 				header("x-tenant","XX").
 				header("Authorization","Basic bWVyY2F0b3I6").
 				header("Content-Type","application/x-www-form-urlencoded").
@@ -29,20 +30,24 @@ public class genericFunctions {
 										ContentType.URLENC)))
 				.contentType("application/x-www-form-urlencoded; charset=UTF-8")
 				.formParam("grant_type", "password")
-				.formParam("username", "PRADEEP")
+				.formParam("username","PRADEEP")
 				.formParam("password", "PASSWORD7").
 				when().post("/uaa/oauth/token").then().extract().response();
 		String r=res.asString();
 		JsonPath js= new JsonPath(r);
 		access_token=js.get("access_token");
-		//System.out.println(access_token);
+		System.out.println(access_token);
 		return access_token;
 	}
 	
 	
 	public static String getProperty(String propname) throws IOException {
 		Properties prop=new Properties();
-		FileInputStream fis=new FileInputStream("C:\\Users\\s00344\\git\\RestAssured\\src\\test\\java\\apicalls\\xxproject.properties");
+		File f= new File("src/test/java/apicalls");
+		File fs= new File(f,"xxproject.properties");
+		String path=fs.getAbsolutePath().toString();
+		System.out.println("Property File read from : "+fs.getAbsolutePath());
+		FileInputStream fis=new FileInputStream(path);
 		prop.load(fis);
 		return prop.get(propname).toString();
 	}
